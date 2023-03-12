7d7bd227d36ddc08b7928cf6f0d74f1e2263f792
Timakz <temirlan_95kz@mail.ru>
2023-03-12 14:56:33 +0600

    Lesson2

--- Program.cs
+++ Program.cs
@@ -4,25 +4,37 @@
     if(arg2 > result) result = arg2;
     if(arg3 > result) result = arg3;
     return result;
-}
-int a1 = 112125;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 2311;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23111;
-int c3 = 313;
+}//             0   1    2   3   4  5   6    7  8
+int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+array[0] = 12;
+//Console.WriteLine(array[4]);
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3],array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+
+
+//int a1 = 112125;
+//int b1 = 21;
+//int c1 = 39;
+//int a2 = 12;
+//int b2 = 2311;
+//int c2 = 33;
+//int a3 = 13;
+//int b3 = 23111;
+//int c3 = 313;
 
 // int max1 = Max(a1, b1, c1);
 // int max2 = Max(a2, b2, c2);
 // int max3 = Max(a3, b3, c3);
 // int max = Max(max1, max2, max3);
 
-int max = Max(
-    Max(a1, b1, c1),
-    Max(a2, b2, c2),
-    Max(a3, b3, c3));
+//int max = Max(
+  //  Max(a1, b1, c1),
+    //Max(a2, b2, c2),
+    //Max(a3, b3, c3));
 
-Console.WriteLine(max);
+//Console.WriteLine(max);
