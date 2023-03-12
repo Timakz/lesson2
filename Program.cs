2503fabc746515283ed6e536321304b24b6926a5
Timakz <temirlan_95kz@mail.ru>
2023-03-12 15:28:27 +0600

    Lesson2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
-    return result;
-}//             0   1    2   3   4  5   6    7  8
-int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-array[0] = 12;
+﻿//int Max(int arg1, int arg2, int arg3)
+//{
+  //  int result = arg1;
+    //if(arg2 > result) result = arg2;
+    //if(arg3 > result) result = arg3;
+    //return result;
+//}             0   1    2   3   4  5   6    7  8
+//int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+//array[0] = 12;
 //Console.WriteLine(array[4]);
 
-int max = Max(
-    Max(array[0], array[1], array[2]),
-    Max(array[3],array[4], array[5]),
-    Max(array[6], array[7], array[8])
-);
-Console.WriteLine(max);
+//int max = Max(
+  //  Max(array[0], array[1], array[2]),
+    // Max(array[3],array[4], array[5]),
+    //Max(array[6], array[7], array[8])
+//);
+//Console.WriteLine(max);
 
 
 //int a1 = 112125;
@@ -38,3 +38,51 @@ Console.WriteLine(max);
     //Max(a3, b3, c3));
 
 //Console.WriteLine(max);
+
+//void FillArray(int[] collection)
+//{
+  //  int length = collection.Length;
+    //int index = 0;
+    //while (index < length)
+    //{
+      //  collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        //index++;
+    //}
+//}
+
+//void PrintArray(int [] col)
+//{
+  //  int count = col.Length;
+    //int position = 0;
+    //while (position < count)
+    //{
+      //  Console.WriteLine(col[position]);
+        //position++;
+    //}
+//}
+
+//int IndexofO(int[] collection, int find)
+//{
+  //  int count = collection.Length;
+    //int index = 0;
+    //int position = 0;
+    //while(index < count)
+    //{
+      //  if(collection[index] == find)
+       // {
+         //   position = index;
+           // break;
+        //}
+        //index++;
+    //}
+    //return position;
+//}
+//int [] array = new int[10];
+
+//FillArray(array);
+//PrintArray(array);
+//Console.WriteLine();
+
+//int pos = IndexofO(array, 4);
+//Console.WriteLine(pos);
